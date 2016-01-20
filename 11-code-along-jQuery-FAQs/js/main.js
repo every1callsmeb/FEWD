4d5e1c4a0ae442c9563835b1419f5ff1d079cec4
Michael Huynh <michael@simfiny.com>
2016-01-19 20:32:49 -0800

    jQuery Toggle Exercise

---
+++ 11-code-along-jQuery-FAQs/js/main.js
@@ -0,0 +1,22 @@
+$('#answer1').hide();
+$('#question1').click(toggleFAQ1);
+
+$('#answer2').hide();
+$('#question2').click(toggleFAQ2);
+
+$('#answer3').hide();
+$('#question3').click(toggleFAQ3);
+
+function toggleFAQ1(){
+  $('#answer1').toggle();
+
+}
+
+function toggleFAQ2(){
+  $('#answer2').fadeToggle();
+}
+
+function toggleFAQ3(){
+
+  $('#answer3').slideToggle();
+}
