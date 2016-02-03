5049f79bf2ef47b3021bb96b642017afc3665c18
Michael Huynh <michael@simfiny.com>
2016-02-02 22:03:02 -0800

    implement next/prev functions

--- 16-code-along-arrays-loops/animals/js/index.js
+++ 16-code-along-arrays-loops/animals/js/index.js
@@ -1,2 +1,34 @@
-var index = 0
+var index = 0 //similar to var total = 0 on cash register exercise
 var images = ["images/animal1.jpg", "images/animal2.jpg", "images/animal3.jpg", "images/animal4.jpg", "images/animal5.jpg", "images/animal6.jpg", "images/animal7.jpg"]
+var newSrc;
+
+$('#next').click(nextImage);
+$('#previous').click(prevImage);
+
+function nextImage(){
+// increment index by 1
+// grab image from array
+// change src tag in html
+
+  console.log('nextImage fired');
+
+  index += 1;  //increments by 1
+  newSrc = images[index];
+
+  $('#image').attr('src', newSrc)
+
+}
+
+function prevImage(){
+// increment index by -1
+// grab image from array
+// change src tag in html
+
+  console.log('prevImage fired');
+
+  index += -1;  //increments by 1
+  newSrc = images[index];
+
+  $('#image').attr('src', newSrc)
+
+}
