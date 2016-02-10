f1b4fb833e3501a78b213666b57cf4ffbd94c8c6
Michael Huynh <michael@simfiny.com>
2016-02-09 16:00:45 -0800

    Add while loop to heatList

--- final-project/js/main.js
+++ final-project/js/main.js
@@ -1,9 +1,40 @@
-// Variables
+// pseudo code
+// set array index based on Heat input
+// for each set manipulate html
+// concatenate "heat" + index
+// add array index in a <li> display in HTML
 
+// set array collection based on heat number input
 
+// Reset Variables
+
+var i = 0 //set counter to 0
+var heatList
+var totalHeats = $("#heats").val();
+
+// Triggers
+
+$("#generateButton").click(calculateHeats);
 
 // Functions
 
+function calculateHeats () {
+
+  var totalHeats = $("#heats").val();
+
+  while (i <= totalHeats) {
+    i = i + 1
+    heatList = '<li>Heat ' + i + '</li>';
+    i++;
+  }
+  console.log(heatList);
+  $("#heatTimes").html(heatList);
+};
 
 
-// Triggers
+
+
+
+
+
+// convert input into proper data type
