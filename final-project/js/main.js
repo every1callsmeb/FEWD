45e8c865b2197a100cd89efcc890e9ab00bfded0
Michael Huynh <michael@simfiny.com>
2016-02-11 12:56:14 -0800

    more work
    styling, add reset function, getting closer to array loop

--- final-project/js/main.js
+++ final-project/js/main.js
@@ -11,37 +11,31 @@
 // var heatList = [1,2,3]
 // var totalHeats
 
+var totalHeats = $("#heats").val();//create counter off heat value to create array
+var heatList = []; //set an empty array that'll be filled via counter loop
+var counter = 0 //counter index
+
 // Triggers
 
 $("#generateButton").click(calculateHeats);
+$("#resetButton").click(reset);
 
 // Functions
 
 function calculateHeats () {
 
-  var totalHeats = $("#heats").val();
-
-  //create counter off heat value to create array
-
-  var heatList = [];
-  var counter = -1 //counter index
-
   while (counter <= totalHeats) {
-    heatList.push('<li>Heat ' + counter + '</li>');
+    heatList.push('<li>Heat ' + counter + '</li>'); //add counter to array group
     counter++; //increment counter
     $("#heatTimes").html('<li>Heat ' + heatList + '</li>' );  //write to html doc
   };
+};
 
-
-    console.log(index);
-
-
-
-
-
-
-
-
-
-
-// convert input into proper data type
+function reset (){
+  counter = 0;
+  heatList = [];
+  $("input[id=heats]").val("");
+  $("input[id=startTime").val("");
+  $("input[id=eventDuration]").val("");
+  $("input[id=transitionTime]").val("");
+}
