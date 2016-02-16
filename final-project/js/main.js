77f4667ccdb4c0a083f96307d9ed4d1467b52c6e
Michael Huynh <michael@simfiny.com>
2016-02-15 22:49:31 -0800

    increment heatTimes

--- final-project/js/main.js
+++ final-project/js/main.js
@@ -45,26 +45,35 @@ function calculateHeats () {
 
   var stringStartTime = $("#startTime").val();
 
+
   // convert stringTime to integer so I can calculate the values
 
   parseTime = stringStartTime.split(":");
   hour = parseTime[0];
   minutes = parseTime[1];
 
-  totalMinutes = (parseInt(hour)*60) + parseInt(minutes);
+  // normalize startTime to millseconds split HH from MM, HH = 60 minutes + MM
+
+  totalMilliseconds = (parseInt(hour)*3600000) + (parseInt(minutes)*60000);
+
+    // console.log("hours"+parseInt(hour)*3600000);
+    // console.log("minutes"+parseInt(minutes)*60000);
+    // console.log(totalMilliseconds);
 
   while (counter < totalHeats) {
-    // normalize startTime to minutes split HH from MM, HH = 60 minutes + MM
-    totalDuration = totalMinutes + Number(eventDuration) + Number(transitionTime);
-    console.log("eventDuration + transitionTime is: "+ totalDuration);
 
-    // i need to convert minutes back into time into the newHeatTime
+    totalDuration = totalMilliseconds + parseInt(eventDuration)*60000 + parseInt(transitionTime)*60000;
+    var newDate = new Date(totalDuration);
 
-    
+    // i need to convert millseconds back into time into the newHeatTime
+    var newHeatTime = newDate.getUTCHours() + ":" + newDate.getUTCMinutes()
 
     counter++; //increment heat number
-    heatList.push('<li>Heat ' + counter + ': <span class= \"heatPadding\" >'+ totalDuration +'</span></li>'); //add counter to array group
+    heatList.push('<li>Heat ' + counter + ': <span class= \"heatPadding\" >'+ newHeatTime +'</span></li>'); //add counter to array group
     $("#heatTimes").html(heatList);  //write to html doc
+
+    // reassign totalMilliseconds to newHeatTime 
+    totalMilliseconds = (parseInt(newDate.getUTCHours())*3600000) + (parseInt(newDate.getUTCMinutes())*60000);
 
   };
   //
